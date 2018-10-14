69726d7619b029ad834a3c30a035b6bca32e1547
En-1 <42154074+En-1@users.noreply.github.com>
2018-10-14 13:37:40 +0300

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,26 @@
+library(dplyr)
+  #creating list of files
+listD<-list.dirs("./data/UCI HAR Dataset")
+listD<-listD[!grepl("Inertial Signals",listD)]
+listF<-unlist(sapply(listD, list.files,full.names = TRUE))
+listF<-listF[grep("txt$",listF)]
+  #loadind files into R
+listN<-gsub("\\.(.*)","",gsub("(.*)/","",listF))
+for (i in 1:length(listF)) {
+    assign(listN[i], try(read.table(listF[i]),silent = TRUE))
+}
+  #1. Merges the training and the test sets to create one data set
+DSet<-rbind(X_test,X_train)
+  #2. Extracts only the measurements on the mean and standard deviation for each measurement
+namesL<-grepl("mean|std",features[,2])
+n<-seq_along(namesL)[namesL]
+DSet<-DSet[,n]
+  #3. Uses descriptive activity names to name the activities in the data set
+Activity<-merge(rbind(y_test,y_train),activity_labels)[,2]
+Subject<-rbind(subject_test,subject_train)
+names(Subject)<-"Subject"
+  #4. Appropriately labels the data set with descriptive variable names
+names(DSet)<-features[,2][namesL]
+DSet<-cbind(Subject,Activity,DSet)
+  #5. creates a second, independent tidy data set with the average of each variable for each activity and each subject
+DSet%>%group_by(Subject,Activity)%>%summarise_all(mean)->DSet1
